9d785af4de24bbe5056eccc3d9b1f50275d1fad3
NICOLAS AVRUTIN <rasputin@ice06.ee.cooper.edu>
2013-04-24 16:48:34 -0400

    stuff that was changed

--- src/main/java/edu/cooper/ece460/authlog/AuthLogReduce.java
+++ src/main/java/edu/cooper/ece460/authlog/AuthLogReduce.java
@@ -18,34 +18,37 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 
-/** AuthLogReduce checks the key (daemon) and calls the appropriate 
-	daemon-specific reduce function */
+/** AuthLogReduce checks the key (daemon) and calls the appropriate
+  daemon-specific reduce function */
 public class AuthLogReduce extends TableReducer<Text, Text, ImmutableBytesWritable> {
-	public static final byte[] hbaseKey = "userhost".getBytes();
+    public static final byte[] cf = "data".getBytes();
+    public static final byte[] localColumn = "local".getBytes();
+    public static final byte[] sshColumn = "ssh".getBytes();
+    public static final byte[] failedColumn = "failed".getBytes();
 
-	@Override
-	public void reduce(Text key, Iterable<Text> values, Context context) 
-		throws IOException, InterruptedException 
-	{
-		int ssh_count = 0, login_count = 0, failed_count = 0;
-		for (Text value : values) {
-			String stringValue = value.toString();
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context)
+    throws IOException, InterruptedException
+{
+    int ssh_count = 0, login_count = 0, failed_count = 0;
+    for (Text value : values) {
+        String stringValue = value.toString();
 
-			if (stringValue.equals("ssh"))
-				ssh_count++;
-			else if (stringValue.equals("login"))
-				login_count++;
-			else if (stringValue.equals("failed"))
-				failed_count++;
-		}
-		int local_login_count = login_count - ssh_count;
+        if (stringValue.equals("ssh"))
+            ssh_count++;
+        else if (stringValue.equals("login"))
+            login_count++;
+        else if (stringValue.equals("failed"))
+            failed_count++;
+    }
+    int local_login_count = login_count - ssh_count;
 
-		Put p = new Put(Bytes.toBytes(key.toString()));
-		p.add(hbaseKey, Bytes.toBytes("local"), Bytes.toBytes(local_login_count));
-		p.add(hbaseKey, Bytes.toBytes("ssh"), Bytes.toBytes(ssh_count));
-		p.add(hbaseKey, Bytes.toBytes("failed"), Bytes.toBytes(failed_count));
-		
-		context.write(null, p);
-	}
+    Put p = new Put(Bytes.toBytes(key.toString()));
+    p.add(cf, localColumn, Bytes.toBytes(local_login_count));
+    p.add(cf, sshColumn, Bytes.toBytes(ssh_count));
+    p.add(cf, failedColumn, Bytes.toBytes(failed_count));
+
+    context.write(null, p);
+}
 }
 
--- src/main/java/edu/cooper/ece460/authlog/HadoopAuthLog.java
+++ src/main/java/edu/cooper/ece460/authlog/HadoopAuthLog.java
@@ -17,28 +17,30 @@ import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 
 public class HadoopAuthLog {
-	public static void main (String[] args) throws Exception {
-		Configuration conf = HBaseConfiguration.create();
-		Path inPath = new Path(args[0]);
+    public static void main (String[] args) throws Exception {
+        Configuration conf = HBaseConfiguration.create();
+        Path inPath = new Path(args[0]);
+        Path outPath = new Path(args[1]);
 
-		Job job1 = new Job(conf, "HadoopAuthLog");
-		job1.setJarByClass(HadoopAuthLog.class);
-		job1.setMapperClass(AuthLogMap.class);
-		job1.setReducerClass(AuthLogReduce.class);
-		job1.setNumReduceTasks(12);
-		job1.setMapOutputKeyClass(Text.class);
-		job1.setMapOutputValueClass(Text.class);
+        Job job1 = new Job(conf, "HadoopAuthLog");
+        job1.setJarByClass(HadoopAuthLog.class);
+        job1.setMapperClass(AuthLogMap.class);
+        job1.setReducerClass(AuthLogReduce.class);
+        job1.setNumReduceTasks(12);
+        job1.setMapOutputKeyClass(Text.class);
+        job1.setMapOutputValueClass(Text.class);
 
-		TableMapReduceUtil.initTableReducerJob(
-			"authlog",        // output table
-			AuthLogReduce.class,    // reducer class
-			job1
-		);
+        TableMapReduceUtil.initTableReducerJob(
+                "authlog",        // output table
+                AuthLogReduce.class,    // reducer class
+                job1
+                );
 
-		job1.setInputFormatClass(TextInputFormat.class);
-		job1.setOutputFormatClass(TextOutputFormat.class);
-		FileInputFormat.addInputPath(job1, inPath);
-		job1.waitForCompletion(true);
-	}
+        job1.setInputFormatClass(TextInputFormat.class);
+        job1.setOutputFormatClass(TextOutputFormat.class);
+        FileInputFormat.addInputPath(job1, inPath);
+        FileOutputFormat.setOutputPath(job1, outPath);
+        job1.waitForCompletion(true);
+    }
 }
 
