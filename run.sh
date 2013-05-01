8210d183f0f78c9a09634fadf2a2c5432fe7e7f3
Max Rothstein <rothst@cooper.edu>
2013-05-01 16:22:17 -0400

    Edited run.sh to include clusters

--- run.sh
+++ run.sh
@@ -10,4 +10,8 @@ rm ${OUT_PATH_LOCAL}/$OUT_PATH || true
 hadoop fs -rmr $OUT_PATH || true
 hadoop jar target/HadoopAuthLog-1.0.jar edu.cooper.ece460.authlog.HadoopAuthLog $IN_PATH $OUT_PATH
 hadoop fs -getmerge $OUT_PATH ${OUT_PATH_LOCAL}/$OUT_PATH
-
+hadoop fs -rmr testdata/authlog_out
+hadoop fs -put authlog_out testdata
+$MAHOUT_HOME/bin/mahout org.apache.mahout.clustering.syntheticcontrol.kmeans.Job
+hadoop fs -get output $OUT_PATH
+$MAHOUT_HOME/bin/mahout clusterdump -i output/clusters-*-final -o out.txt
